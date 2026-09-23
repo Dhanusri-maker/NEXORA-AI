17b268a9b360f8a7b16554776164435505b7ee3f
Dhanu Sri <srirdhanusri@gmail.com>
2026-09-23 22:27:32 +0530

    fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
         icons: [
           
           {
-            src: "/nexora-icon.png",
+            src: "/nexora-icon.png.jpeg",
             sizes: "512x512",
             type: "image/png",
           },
